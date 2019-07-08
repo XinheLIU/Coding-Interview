33d778e033d366afa5f05fbb02f52d29189ca721
XinheLIU <LIUXinhe@outlook.com>
2019-07-08 08:18:48 -0400

    linked list

--- Python/Data Structure/Linked List/Split Linked List in Parts.py
+++ Python/Data Structure/Linked List/Split Linked List in Parts.py
@@ -21,3 +21,27 @@ class Solution:
                 if p: p = p.next
             ret.append(head.next)
         return ret
+        
+'''
+# in memory version 
+class Solution:
+    def splitListToParts(self, root: 'ListNode', k: 'int') -> 'List[ListNode]':
+        # count length
+        p1, N = root, 0
+        while p1:
+            p1 = p1.next
+            N += 1
+        ret, p = [], root
+        width, res = divmod(N, k)
+        for i in range(k):
+            # dummy head
+            head = write = ListNode(None)
+            for _ in range(1, width + (i < res)):
+                write.next = p
+                write = write.next
+                if p: p = p.next
+            write.next = p
+            if p: p.next, p = None, p.next
+            ret.append(head.next)
+        return ret
+'''
