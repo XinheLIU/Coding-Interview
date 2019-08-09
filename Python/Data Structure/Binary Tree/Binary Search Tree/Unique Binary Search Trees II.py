e0fae0e2ab0776bc50711ffe58e5557d20f44b99
XinheLIU <LIUXinhe@outlook.com>
2019-08-08 23:58:28 -0400

    DP

--- Python/Data Structure/Binary Tree/Binary Search Tree/Unique Binary Search Trees II.py
+++ Python/Data Structure/Binary Tree/Binary Search Tree/Unique Binary Search Trees II.py
@@ -24,3 +24,25 @@ class Solution:
                     for right in trees(root + 1, end)] or [None]
             return memo[start, end]
         return trees(1, n)
+        
+"""
+#
+class Solution:
+    def generateTrees(self, n: int) -> List[TreeNode]:
+        if n == 0: return []
+        return self.dfs(1, n)
+        
+    def dfs(self, start, end):
+        if start > end: return [None]
+        res = []
+        for rootval in range(start, end+1):
+            LeftTree = self.dfs(start, rootval-1)
+            RightTree = self.dfs(rootval + 1, end)
+            for i in LeftTree:
+                for j in RightTree:
+                    root = TreeNode(rootval)
+                    root.left = i
+                    root.right = j
+                    res.append(root)
+        return res
+"""
--- Python/Data Structure/Binary Tree/Binary Search Tree/Unique Binary Search Trees.py
+++ Python/Data Structure/Binary Tree/Binary Search Tree/Unique Binary Search Trees.py
@@ -1,9 +1,5 @@
 class Solution:
-    def numTrees(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+    def numTrees(self, n: int) -> int:
         # Dynamic Programming : Catalan Number
         dp = [0] * (n+1)
         dp[0], dp[1] = 1, 1
