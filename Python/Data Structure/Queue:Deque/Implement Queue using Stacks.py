67956a751923a9427de84db34689756ee9734275
XinheLIU <LIUXinhe@outlook.com>
2019-08-18 18:21:47 -0400

    stack

--- Python/Data Structure/Queue:Deque/Implement Queue using Stacks.py
+++ Python/Data Structure/Queue:Deque/Implement Queue using Stacks.py
@@ -1,50 +1,46 @@
-class MyQueue(object):
+class MyQueue:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.stack1 = [] # input
-        self.stack2 = [] # output
+        self.stack1 = []
+        self.stack2 = []
         
-    def push(self, x):
-        """
-        Push element x to the back of queue.
-        :type x: int
-        :rtype: void
-        """
-        self.stack1.append(x)
-        
+
     def adjust(self):
         if not len(self.stack2):
             while len(self.stack1):
                 self.stack2.append(self.stack1.pop())
+                
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        # input stack
+        self.stack1.append(x)
 
-    def pop(self):
+    def pop(self) -> int:
         """
         Removes the element from in front of queue and returns that element.
-        :rtype: int
         """
+        # output stack
         self.adjust()
         return self.stack2.pop()
-        
 
-    def peek(self):
+    def peek(self) -> int:
         """
         Get the front element.
-        :rtype: int
         """
         self.adjust()
         if len(self.stack2):
-            return self.stack2[-1]
-        
+            return self.stack2[-1]     
 
-    def empty(self):
+    def empty(self) -> bool:
         """
         Returns whether the queue is empty.
-        :rtype: bool
         """
-        return self.stack1 == [] and self.stack2 == []
+        return not len(self.stack1) and not len(self.stack2)
 
 
 # Your MyQueue object will be instantiated and called as such:
