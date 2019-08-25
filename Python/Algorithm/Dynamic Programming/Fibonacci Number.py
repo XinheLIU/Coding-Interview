2bb27401111fb57dbda8e6b8952bfc860799a5b9
XinheLIU <LIUXinhe@outlook.com>
2019-08-24 22:25:55 -0400

    dp

--- Python/Algorithm/Dynamic Programming/Fibonacci Number.py
+++ Python/Algorithm/Dynamic Programming/Fibonacci Number.py
@@ -1,13 +1,7 @@
 class Solution:
-    def fib(self, N):
-        """
-        :type N: int
-        :rtype: int
-        """
-        if N <= 1:
-            return 0 if not N else 1
-        else:
-            x, y = 0, 1 #f0, f1
-            for _ in range(N-1): #starts from f2
-                x, y = y, x + y
-            return y
+    def fib(self, N: int) -> int:
+        if N == 0: return 0
+        x, y = 1, 1
+        for i in range(2, N):
+            x, y = x + y, x
+        return x
--- Python/Algorithm/Dynamic Programming/Min Cost Climbing Stairs.py
+++ Python/Algorithm/Dynamic Programming/Min Cost Climbing Stairs.py
@@ -1,11 +1,6 @@
 class Solution:
-    def minCostClimbingStairs(self, cost):
-        """
-        :type cost: List[int]
-        :rtype: int
-        """
-        # Fabonacci
-        dp = [0] * (len(cost) + 1) # cost -1, 0, ...N
-        for i in range(2,len(cost) + 1):
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp = [0] * (len(cost) + 1)
+        for i in range(2, len(cost) + 1):
             dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
         return dp[-1]
