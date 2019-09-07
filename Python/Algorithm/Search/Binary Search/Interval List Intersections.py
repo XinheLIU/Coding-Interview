a3d4201b90a9fffdacdc32eda005b5eb102fc916
XinheLIU <LIUXinhe@outlook.com>
2019-09-07 16:21:20 -0400

    update

---
+++ Python/Algorithm/Search/Binary Search/Interval List Intersections.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def intervalIntersection(self, A, B):
+        ret = []
+        i = j = 0
+
+        while i < len(A) and j < len(B):
+            # Let's check if A[i] intersects B[j].
+            # lo - the startpoint of the intersection
+            # hi - the endpoint of the intersection
+            lo = max(A[i][0], B[j][0])
+            hi = min(A[i][1], B[j][1])
+            if lo <= hi:
+                ret.append([lo, hi])
+
+            # Remove the interval with the smallest endpoint
+            if A[i][1] < B[j][1]:
+                i += 1
+            else:
+                j += 1
+
+        return ret
