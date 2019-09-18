92dda117bd1374cbf62c3effacc16bc60782cf7e
XinheLIU <LIUXinhe@outlook.com>
2019-09-18 07:24:39 -0400

    two pointers

---
+++ Python/Data Structure/Array/Two Pointers/Shortest Word Distance III.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def shortestDistance(self, words, word1, word2):
+        """
+        :type words: List[str]
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        dis = sys.maxint
+        ind = [None, None]
+        if len(words) == 0:
+            return dis
+        for i in range(len(words)):
+        	prev = ind[0]
+            if words[i] == word1:
+                ind[0] = i
+            elif words[i] == word2:
+                ind[1] = i
+            if ind[0] != None and ind[1] != None:
+            	if word1 == word2 and prev != -1 and prev != ind[0]:
+            		dis = min(dis, abs(ind[0] - t))
+            	elif ind[0] != ind[1]:
+                	dis = min(dis, abs(ind[0] - ind[1]))
+        return dis
