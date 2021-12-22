f65020b72e0da61bc37a40b03ae2767dcf037386
XINHE LIU <LIUXinhe@outlook.com>
2021-12-22 21:50:43 +0800

    sql

--- Sql/176.second-highest-salary.sql
+++ Sql/176.second-highest-salary.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-# SELECT MAX(Salary) AS SecondHighestSalary FROM Employee WHERE Salary < (SELECT MAX(Salary) FROM Employee)
+---SELECT MAX(Salary) AS SecondHighestSalary FROM Employee WHERE Salary < (SELECT MAX(Salary) FROM Employee)
 SELECT
     (SELECT DISTINCT
             Salary
@@ -8,3 +8,20 @@ SELECT
         ORDER BY Salary DESC
         LIMIT 1 OFFSET 1) AS SecondHighestSalary
 ;
+/*
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key column for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.
+
+The query result format is in the following example.
+*/
